e4c9a1c493b93875fd5f664382466f2297b83682
Lee SangHyup <bosuksh@gmail.com>
2021-03-07 00:10:56 +0900

    feat: signUp 기능 테스트
    테스트시 csrf 필터를 사용하면 테스트코드에 같이 넣어줘야한다.

--- src/main/java/me/doflamingo/demospringsecurityform/form/SignUpController.java
+++ src/main/java/me/doflamingo/demospringsecurityform/form/SignUpController.java
@@ -1,18 +1,34 @@
 package me.doflamingo.demospringsecurityform.form;
 
 import me.doflamingo.demospringsecurityform.account.Account;
+import me.doflamingo.demospringsecurityform.account.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/signup")
 public class SignUpController {
 
+  private final AccountService accountService;
+
+  public SignUpController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
   @GetMapping
-  public String signupForm(Model model) {
+  public String signUpForm(Model model) {
     model.addAttribute("account", new Account());
     return "signup";
+  }
+
+  @PostMapping
+  public String signUp(@ModelAttribute Account account) {
+    account.setRole("USER");
+    accountService.createNewAccount(account);
+    return "redirect:/login";
   }
 }
--- src/test/java/me/doflamingo/demospringsecurityform/form/SignUpControllerTest.java
+++ src/test/java/me/doflamingo/demospringsecurityform/form/SignUpControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,5 +34,21 @@ class SignUpControllerTest {
       .andDo(print())
       .andExpect(status().isOk())
       .andExpect(content().string(containsString("_csrf")));
+  }
+
+  @Test
+  @DisplayName("sign up 성공 테스트")
+  public void signUp() throws Exception {
+    //given
+    String username = "doflamingo";
+    String password = "123";
+    //when
+    mockMvc.perform(post("/signup")
+      .param("username", username)
+      .param("password", password)
+      .with(csrf()))
+    //then
+    .andDo(print())
+    .andExpect(status().is3xxRedirection());
   }
 }
